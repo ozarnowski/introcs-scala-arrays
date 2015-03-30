90f47ea994150be943e75cac189a78d87f3e6590
ozarnowski <ozarnowski@luc.edu>
2015-03-30 16:36:14 +0000

    hello

--- arrays_tests.scala
+++ arrays_tests.scala
@@ -15,19 +15,20 @@ class RationalScalaTestFlatSpecMatchers extends FlatSpec with Matchers {
 
   it should "handle non-empty array" in {
     val result = getIntsAsString("label2: ", ":", Array[Int](1, 2, 3))
-    result should be ("label1: 1:2:3")
+    result should be ("label2: 1:2:3")
   }
 
   it should "handle different labels" in {
     val result = getIntsAsString("label2: ", ":", Array[Int](1, 2, 3))
-    result should be ("label1: 1:2:3")
+    result should be ("label2: 1:2:3")
   }
 
   "minimum()" should "not work for 0-length array" in {
     intercept[IllegalArgumentException] {
       val minValue = minimum(Array[Int]())
-    }
+         }
   }
+    
 
   it should "work for a >0-length array" in {
     val minValue = minimum(Array(5, 10, 2, 4, 8, 6, -1, -3))
@@ -74,9 +75,9 @@ class RationalScalaTestFlatSpecMatchers extends FlatSpec with Matchers {
   "pairwiseAdd()" should "add corresponding elements of arrays" in {
     val a = Array(1, 2, 4, 6, 8, 10)
     val b = Array(1, 2, 4, 6, 8, 10, 13)
-    val result = Array.fill(5)(0)
+    val result = Array.fill(6)(0)
     pairwiseAdd(a, b, result)
-    result should be (Array(2, 4, 8, 12, 16))
+    result should be (Array(2, 4, 8, 12, 16, 20))
   }
 
   "newPairwiseAdd()" should "add corresponding elements of arrays" in {
@@ -108,7 +109,7 @@ class RationalScalaTestFlatSpecMatchers extends FlatSpec with Matchers {
   }
 
   it should "also work with different data" in {
-    val a = Array(2, 5, 8, 3, 9, 9, 8, 3, 3, 4, 4, 5, 5, 6, 6)
+    val a = Array(2, 5, 8, 3, 9, 9, 8, 3, 3, 2, 6, 5, 5, 1, 2)
     getAscendingRun(a, 0) should be (3)
     getAscendingRun(a, 3) should be (6)
     getAscendingRun(a, 6) should be (7)
